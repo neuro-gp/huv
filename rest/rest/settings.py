51ad372f04e886f89c3636b02f750ca90bf10ed0
Guido Pascariello <guidop@kozaca.com.ar>
2020-11-28 18:18:05 -0300

    ajustes

--- rest/rest/settings.py
+++ rest/rest/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from app.utils import get_env_value
+from rest.utils import get_env_value
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
