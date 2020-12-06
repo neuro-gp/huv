63c596ae004b8116cfb30a64e9efcf70c1e1899f
Guido Pascariello <guidop@kozaca.com.ar>
2020-12-06 19:24:35 -0300

    admin data

--- rest/plantas/admin.py
+++ rest/plantas/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from plantas import models
+
+admin.site.register(models.Familia)
+admin.site.register(models.Rotaciones)
+admin.site.register(models.Epoca)
+admin.site.register(models.Fuente)
+admin.site.register(models.Tip)
+admin.site.register(models.Sustrato)
+admin.site.register(models.Tipo)
+admin.site.register(models.Planta)
+admin.site.register(models.Ficha)
+admin.site.register(models.Interacciones)
